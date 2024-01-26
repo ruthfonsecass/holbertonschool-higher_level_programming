144ffbdfdcf4562d887425bcc60cbea3905f407a
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-01-26 07:56:00 -0800

    update

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,5 +8,5 @@ if last_digit > 5:
     text += "and is greater than 5"
 elif last_digit == 0:
     text += "and is 0"
-elif last_digit < 6:
+elif 0 < last_digit < 6:
     text += "and is less than 6 and not 0"
