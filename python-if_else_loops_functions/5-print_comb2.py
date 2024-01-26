51d4a091ff93e5bf315c26519f4cadc0c648561f
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-01-26 10:22:42 -0800

    update

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
-    if num < 10:
-        print(f"0{num}")
+    if num == 99:
+        print("{:02d}".format(i))
     else:
-        print(num)
+        print("{:02d}".format(i), end=", ")
