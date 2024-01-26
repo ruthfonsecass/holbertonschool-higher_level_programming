54e7fd6e6a338bd4a8e15b5d66bd53992c3c4e5e
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-01-26 08:05:31 -0800

    update

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
-text = "Last digit of {} is {}".format(number, last_digit)
 
 if last_digit > 5:
-    text += "and is greater than 5"
+    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
 elif last_digit == 0:
-    text += "and is 0"
+    print("Last digit of {} is {} and is 0".format(number, last_digit))
 elif 0 < last_digit < 6:
-    text += "and is less than 6 and not 0"
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
