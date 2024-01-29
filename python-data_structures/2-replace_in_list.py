83951c240c0962dd255d11281de6c9eba88fa4ba
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-01-29 12:09:57 -0800

    replace

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx is not None and 0<= idx < len(my_list):
+    if idx is None and 0 <= idx < len(my_list):
         return my_list
 
     new_list = my_list.replace("idx", "element")
