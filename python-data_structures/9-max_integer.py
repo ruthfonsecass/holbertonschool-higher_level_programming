7fa97a47d52bdb0243a363c19c3b3e36407556b7
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-01-30 13:03:43 -0800

    max integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,9 +3,8 @@ def max_integer(my_list=[]):
     if my_list == "":
         return None
     
-    max_number = my_list
-    for x in my_list:
+    max_number = my_list[0]
+    for x in range my_list:
         if x > max_number:
             max_number = x
-            print("{:d}".format(max_number))
-
+    return max_number
