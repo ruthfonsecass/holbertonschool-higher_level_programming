c3a10486a72f47b3c00b675f287712279dac7754
root <git@github.com:ruthfonsecass/MySuperRepo.git>
2024-02-01 00:51:53 -0800

    task

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(my_list):
+    for i in my_list:
         if my_list[i] == search:
             my_list.replace(search, replace)
