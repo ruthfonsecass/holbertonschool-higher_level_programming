f003b308530850b3f161a43fce07170e8e1807a2
ruthfonsecass <7502@holbertonstudents.com>
2024-02-02 14:48:52 +0100

    task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx is None or idx >= len(my_list):
-        return
-    if idx is my_list:
-        return my_list.idx
+    if idx < 0 or idx >= len(my_list):
+        return None
+    if idx in my_list:
+        return my_list[idx]
