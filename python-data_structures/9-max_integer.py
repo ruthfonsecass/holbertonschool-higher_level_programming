2b35d5229fe2951d26767e3453a6df7cf630b17a
ruthfonsecass <7502@holbertonstudents.com>
2024-02-02 18:56:28 +0100

    update

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -4,7 +4,7 @@ def max_integer(my_list=[]):
         return None
     
     max_number = my_list[0]
-    for x in range my_list:
-        if x > max_number:
-            max_number = x
+    for x in range(len(my_list)):
+        if my_list[x] > max_number:
+            max_number = my_list[x]
     return max_number
