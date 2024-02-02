8dce54fb3b003b1fed8ddb8e951c947d2d8d3d29
ruthfonsecass <7502@holbertonstudents.com>
2024-02-02 22:37:38 +0100

    update

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,9 +3,8 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end='')
-        print()
     except IndexError:
-        return x
+        x = i
     finally:
         print()
         return x
