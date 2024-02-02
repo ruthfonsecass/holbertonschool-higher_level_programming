056aa71e3aa968251d1597a203130854107f0243
ruthfonsecass <7502@holbertonstudents.com>
2024-02-02 15:21:06 +0100

    ficher

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,5 +2,5 @@
 def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return None
-    if idx in my_list:
+    else:
         return my_list[idx]
--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx is None and 0 <= idx < len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return my_list
 
     new_list = my_list.replace("idx", "element")
