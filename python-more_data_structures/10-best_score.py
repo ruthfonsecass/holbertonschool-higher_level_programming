0bb12dd60c43e62cd28ea20b83c41a893032878b
ruthfonsecass <7502@holbertonstudents.com>
2024-02-02 22:26:52 +0100

    update

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return None
     else:
-        return max(a_dictionary, key=a_dictionary)
+        return max(a_dictionary, key=a_dictionary.get)
