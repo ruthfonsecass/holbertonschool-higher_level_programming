921d68c0e9e5b0438fc7efff7f159d05ee44ff97
ruthfonsecass <7502@holbertonstudents.com>
2024-02-05 21:43:14 +0100

    update

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+class Square:
+    def _init__ (self, size=0):
+        if isinstance(size, int):
+            self.size = size
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
