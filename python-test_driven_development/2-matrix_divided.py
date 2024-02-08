49fcefdcfcaa62ed691cc55031b7d49502ba81c6
ruthfonsecass <7502@holbertonstudents.com>
2024-02-08 22:44:41 +0100

    update

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def matrix_divided(matrix, div):
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def print_square(size):
+    if type(size) not in int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
