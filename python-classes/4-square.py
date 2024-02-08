6a3bfe32ead388b4680ee547b5a6e276d36dd0ae
ruthfonsecass <7502@holbertonstudents.com>
2024-02-08 09:43:59 +0100

    update

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+class Square:
+    def _init__ (self, size=0):
+        self._size = size
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.size = value
+    def area(self):
+        return self._size * self._size
