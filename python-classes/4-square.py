a23dc228aae137d615a861185e2beb2af48b0461
ruthfonsecass <7502@holbertonstudents.com>
2024-02-08 10:11:44 +0100

    update

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 class Square:
     def _init__ (self, size=0):
-        self._size = size
+        self.__size = size
     @property
     def size(self):
-        return self._size
+        return self.__size
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -12,6 +12,6 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.size = value
+            self.__size = value
     def area(self):
-        return self._size * self._size
+        return self.__size * self.__size
--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+@property
+def size(self):
+    return self.__size
+
+@size.setter
+def size(self, value):
+    if not isinstance(value, int):
+        raise TypeError("size must be an integer")
+    elif value < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        self.__size = value
+def area(self):
+    return self.__size * self.__size
+
+def my_print(self):
+    if not self.size:
+        print()
+    else:
+        for i in range(sel.size):
+            for j in range(self.size):
+                print("#", end="")
+            print()
