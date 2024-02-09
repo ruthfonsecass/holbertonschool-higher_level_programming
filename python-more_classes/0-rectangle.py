03acd75233f4ad0327f9cebb48d0ef38ecbce293
ruthfonsecass <7502@holbertonstudents.com>
2024-02-09 23:25:07 +0100

    update

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 class Rectangle:
     pass
+Les projets se ferment officiellement pour tout le monde ce dimanche à 23h 59min 59s (fondamentaux uniquement). @Active Student (modifié)
+￼
+8
+￼
+1
+￼
+1
+￼
+1
+￼
+1
+[18:29]
