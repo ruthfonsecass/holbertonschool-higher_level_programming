2f99ebf50e48fbc1a5e6e22320b2179c38a503af
ruthfonsecass <7502@holbertonstudents.com>
2024-02-09 13:33:17 +0100

    update

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError ("text must be a string")
+    for i in range(len(text)):
+        print(text[i], end="")
+        if text[i] in ".?:":
+            print("\n\n", end,"")
+        if i + 1 < len(text) and text[i + 1] == " ":
+            print(end="")
