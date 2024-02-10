a392c392f5cb429167d58385176e9a16361451b9
ruthfonsecass <7502@holbertonstudents.com>
2024-02-10 11:11:05 +0100

    update

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """Define Python interpreter path."""
+
+
 class Rectangle:
     """Class definition for a rectangle."""
     def __init__(self, width=0, height=0):
+
         """Initialize a new Rectangle instance with width and height."""
         self.__width = width
         self.__height = height
+
 
 @property
 def width(self):
     """Get the height of the rectangle."""
     return self.__width
+
 
 @width.setter
 def width(self, value):
@@ -21,13 +26,15 @@ def width(self, value):
     else:
         self.__width = value
 
+
 @property
 def height(self):
     return self.__height
 
+
 @height.setter
 def height(self, value):
-    """Set the height of the rectangle, ensuring it's a non-negative integer."""
+    """Set the height of the rectangle."""
     if type(value) is not int:
         raise TypeError("height must be an integer")
     if value < 0:
@@ -35,9 +42,11 @@ def height(self, value):
     else:
         self.__height = value
 
+
 def area(self):
     """Calculate and return the area of the rectangle."""
     return self.__height * self.__width
+
 
 def perimeter(self):
     """Calculate and return the perimeter of the rectangle."""
