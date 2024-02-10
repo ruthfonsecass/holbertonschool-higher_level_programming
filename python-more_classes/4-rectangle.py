56eaba089a3772117c855a6c9aa78f9d32151150
ruthfonsecass <7502@holbertonstudents.com>
2024-02-10 11:26:49 +0100

    update

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -5,14 +5,15 @@
 class Rectangle:
     """Class that defines a rectangle"""
 
-
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
 
+
 @property
 def width(self):
     return self.__width
+
 
 @width.setter
 def width(self, value):
@@ -23,9 +24,11 @@ def width(self, value):
     else:
         self.__width = value
 
+
 @property
 def height(self):
     return self.__height
+
 
 @height.setter
 def height(self, value):
@@ -36,8 +39,10 @@ def height(self, value):
     else:
         self.__height = value
 
+
 def area(self):
     return self.__height * self.__width
+
 
 def perimeter(self):
     if self.__height == 0 and self.__width == 0:
@@ -45,16 +50,18 @@ def perimeter(self):
     else:
         return 2 * (self.__height + self.__width)
 
+
 def __str__(self):
     rectangle = ""
 
     if self.width == 0 or self.height == 0:
         return rectangle
-    
+
     for i in range(self.height):
         rectangle += ("#" * self.width) + "\n"
 
     return rectangle[:-1]
+
 
 def __repr__(self):
     return "Rectangle({:d}{:d})".format(self.heigth, self.width)
