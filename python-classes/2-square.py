fdcf6ca72bb5eaf3dc2a78e8b7b7b900da803002
ruthfonsecass <7502@holbertonstudents.com>
2024-02-10 21:18:38 +0100

    update

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """Specify Python interpreter path."""
 
+
 class Square:
     """Defines a class Square."""
-    
+
     def __init__(self, size=0):
         """Initializes Square with a default size of 0, ensuring size is an int and >= 0."""
-        
+
         if not isinstance(size, int):
             """Check if size is not an integer."""
             raise TypeError("size must be an integer")
-            
+  
         if size < 0:
             """Check if size is less than 0."""
             raise ValueError("size must be >= 0")
