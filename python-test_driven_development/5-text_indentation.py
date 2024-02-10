4fb0a279b7a07e23e9fb7d61b9839eec06b14a1f
ruthfonsecass <7502@holbertonstudents.com>
2024-02-10 10:41:28 +0100

    update

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 def text_indentation(text):
+    """ Function that prints 2 new lines after ".?:" characters
+
+    Args:
+        text: input string
+
+    Returns:
+        No return
+
+    Raises:
+        TypeError: If text is not a string
+    """
     if not isinstance(text, str):
         raise TypeError ("text must be a string")
     for i in range(len(text)):
+        if text[i - 1] in ".?:" and i > 0 and text[i] is " ":
+            continue
         print(text[i], end="")
-        if text[i] in ".?:":
-            print("\n\n", end,"")
-        if i + 1 < len(text) and text[i + 1] == " ":
-            print(end="")
+        if text[i] is "." or text[i] is "?" or text[i] is ":":
+            print("\n")
