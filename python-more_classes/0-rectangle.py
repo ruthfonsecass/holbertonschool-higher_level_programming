988aaffb65a969a393e5949c2a837cd2ab281df4
ruthfonsecass <7502@holbertonstudents.com>
2024-02-10 10:46:54 +0100

    update

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+
+This module provides a simple rectangle class.
+"""
 class Rectangle:
     """Class definition for a rectangle."""
     pass
