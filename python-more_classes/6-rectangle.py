867d56ac64e126a6feef2b23c225141166f0a537
ruthfonsecass <7502@holbertonstudents.com>
2024-02-10 11:46:37 +0100

    update

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -11,6 +11,8 @@ This module is composed by a class that defines a Rectangle
 class Rectangle:
     """ Class that defines a rectangle """
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Method that initializes the instance
 
