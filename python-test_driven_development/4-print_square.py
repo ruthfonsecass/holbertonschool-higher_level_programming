a7b7a0d0744042941fa00a7ca3cd40eaf23a32c1
ruthfonsecass <7502@holbertonstudents.com>
2024-02-10 10:32:57 +0100

    update

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+"""Define Python interpreter path."""
+
 def print_square(size):
+    """Prints a square of a given size using the '#' character.
+
+    Args:
+        size (int): The size of the square's sides. Must be an integer >= 0.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is negative.
+    """
     if type(size) not in int:
         raise TypeError("size must be an integer")
     if size < 0:
+    
         raise ValueError("size must be >= 0")
 
     for i in range(size):
