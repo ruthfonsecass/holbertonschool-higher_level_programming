e2f43e864454bea94bf9d8322d22729747ad5dde
ruthfonsecass <7502@holbertonstudents.com>
2024-02-14 00:48:32 +0100

    update

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,6 +3,4 @@
 class MyList(list):
     """A class list that inherits from list"""
     def print_sorted(self):
-        for i in sorted(self):
-            print(i, end='')
-        print()
+        print(sorted(self))
