315f8f2898134d1450f16d6874cb5ed3683694aa
ruthfonsecass <7502@holbertonstudents.com>
2024-02-13 01:33:46 +0100

    ficher

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -52,7 +52,21 @@ class Rectangle:
         if self.__height != 0 and self.__width != 0:
             for i in range(self.__height):
                 for j in range(self.__width):
-                    string += "#"
+                   Write a blog post about everything you just learned / this project is covering. Your blog post should be articulated this way (one paragraph per item):
+
+introduction
+id and type
+mutable objects
+immutable objects
+why does it matter and how differently does Python treat mutable and immutable objects
+how arguments are passed to functions and what does that imply for mutable and immutable objects
+If you worked on advanced tasks, please also include what you did learn in those tasks in the blog post.
+
+Your posts should have many code/output examples to illustrate what you are explaining, and at least one picture, at the top. Publish your blog post on Medium or LinkedIn, and share it at least on LinkedIn.
+
+When done, please add all urls below (blog post, LinkedIn post, etc.)
+
+Please, remember that these blogs must be written in English to further your technical ability in a variety of settings. string += "#"
                 if i < self.__height - 1:
                     string += "\n"
 
