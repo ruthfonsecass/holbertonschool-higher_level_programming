de003d2f62171c0ddb737655f556c05527fc1e08
ruthfonsecass <7502@holbertonstudents.com>
2024-02-14 00:39:40 +0100

    update

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-classe MyList(list):
-def print_sorted(self):
-    for i in sorted(self):
-        print(i, end='')
-    print()
+"""A module with a class MyList that inherits from class list"""
+class MyList(list):
+    """A class list that inherits from list"""
+    def print_sorted(self):
+        for i in sorted(self):
+            print(i, end='')
+        print()
