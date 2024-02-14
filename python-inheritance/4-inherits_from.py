e9a2c21a604558a23ccaf3543ed4c7c71872e59f
ruthfonsecass <7502@holbertonstudents.com>
2024-02-14 02:22:49 +0100

    update

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """A module with a class MyList that inherits from class list"""
+
+
 def inherits_from(obj, a_class):
-    """function that returns True if the object is an instance of a class 
-    that inherited (directly or indirectly) 
+    """function that returns True if the object is an instance of a class
+    that inherited (directly or indirectly)
     from the specified class ; otherwise False
     """
     if issubclass(obj, a_class):
