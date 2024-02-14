f887b271d30c9ecca2961b2c73c6aa3f592cc2da
ruthfonsecass <7502@holbertonstudents.com>
2024-02-14 01:59:02 +0100

    update

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""A module with a class MyList that inherits from class list"""
+def is_kind_of_class(obj, a_class):
+    """function that returns True if the object is exactly an instance
+    of the specified class ; 
+    otherwise False.
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
