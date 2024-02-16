53ab23c012be5b3d14fd4a2c3069ed1b07f266e0
ruthfonsecass <7502@holbertonstudents.com>
2024-02-16 21:02:20 +0100

    update

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """"A module with an empty BaseGeometry class"""
+
+
 class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+""""A module with an empty BaseGeometry class"""
+
+
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+
+def integer_validator(self, name, value):
+    self.name = name
+    self.value = value
+
+    if type(value) is not int:
+        raise TypeError("{} must be an integer".format(name))
+    if value <= 0:
+        raise ValueError("{} must be greater than 0".format(name))
