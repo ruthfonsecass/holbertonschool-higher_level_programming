36f2f457431e3da49e5253ba24c7ae1696adee3a
ruthfonsecass <7502@holbertonstudents.com>
2024-02-18 15:38:14 +0100

    update

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -9,4 +9,8 @@ import json
 
 
 def to_json_string(my_obj):
+    """
+    Function that returns an object (Python data structure)
+    represented by a JSON string.
+    """
     return json.dumps(my_obj)
