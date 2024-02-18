4984f836a8219725c15c04cd05ea4ed90f547cd8
ruthfonsecass <7502@holbertonstudents.com>
2024-02-18 15:36:13 +0100

    update

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -10,5 +10,5 @@ def write_file(filename="", text=""):
     Function that writes a string to a text file
     and returns the number of characters written.
     """
-    with open(filename, 'w', encoding="") as file:
+    with open(filename, 'w', encoding="utf-8") as file:
         return(file.write(text))
