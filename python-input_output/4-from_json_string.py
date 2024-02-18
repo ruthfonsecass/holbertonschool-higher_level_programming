d73144ed87b441f15d951754c7ce5bcf2ff756e7
ruthfonsecass <7502@holbertonstudents.com>
2024-02-18 15:44:21 +0100

    update

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -13,4 +13,4 @@ def from_json_string(my_str):
     Function that returns an object (Python data structure)
     represented by a JSON string.
     """
-    return json.dumps(my_str)
+    return (json.dumps(my_str))
