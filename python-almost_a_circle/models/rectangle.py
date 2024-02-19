844e0a99d7e19ab86d193cccb136da614185e93c
ruthfonsecass <7502@holbertonstudents.com>
2024-02-20 00:58:25 +0100

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,9 +18,8 @@ class Rectangle(Base):
             y (int): the y position of the Rectangle
         """
         super().__init__(id)
-
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         self.x = x
         self.y = y
 
@@ -34,7 +33,7 @@ def width(self):
 
 @width.setter
 def width(self, value):
-    if type(value) is not (int):
+    if type(value) is not int:
         raise TypeError("width must be an integer")
     if width <= 0:
         raise ValueError("width must be > 0")
@@ -50,7 +49,7 @@ def heigth(self):
 
 @heigth.setter
 def heigth(self, value):
-    if type(value) is not (int):
+    if type(value) is not int:
         raise TypeError("heigth must be an integer")
     if width <= 0:
         raise ValueError("heigth must be > 0")
@@ -66,7 +65,7 @@ def x(self):
 @x.setter
 def x(self, value):
     """Sets the height of the Rectangle"""
-    if type(value) is not (int):
+    if type(value) is not int:
         raise TypeError("x must be an integer")
     if x < 0:
         raise ValueError("x must be >= 0")
@@ -81,7 +80,7 @@ def y(self):
 
 @y.setter
 def y(self, value):
-    if type(value) is not (int):
+    if type(value) is not int:
         raise TypeError("y must be an integer")
     if y < 0:
         raise ValueError("y must be >= 0")
