6da33572767c9c749550d1ddb2c94d8c36bbfa25
ruthfonsecass <7502@holbertonstudents.com>
2024-02-19 23:24:55 +0100

    update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""""A module with a simple class"""""
 class Base:
+    """This class will be the “base” of all other classes in this project.
+    The goal of it is to manage id attribute in all your future classes and
+    to avoid duplicating the same code (by extension, same bugs)
+    """
     __nb_objects = 0
 
 
