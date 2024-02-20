1b8d23f4dca89eb5d992953a348c815a3c8f3b45
ruthfonsecass <7502@holbertonstudents.com>
2024-02-20 01:11:15 +0100

    update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -25,5 +25,5 @@ def __init__(self, id=None):
     if id is not None:
         self.id = id
     else:
-        type(self).__nb_objects += 1
+        Base.__nb_objects += 1
         self.id = Base.__nb_objects
