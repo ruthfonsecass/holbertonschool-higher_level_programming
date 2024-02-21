13cdb6c142528738ca3168fb7cf8f8dc7e36ad9d
ruthfonsecass <7502@holbertonstudents.com>
2024-02-21 16:12:00 +0100

    update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -10,20 +10,20 @@ class Base:
     __nb_objects = 0
 
 
-def __init__(self, id=None):
-    """
-        This is a constructor function that assigns an ID to an object either
-        based on a given ID or by incrementing a counter.
-
-        Args:
-            id (int): The "id" parameter is an optional argument that can
-            be passed to the constructor of an object. If a value is provided
-            for "id", it will be assigned to the object's "id" attribute.
-            If no value is provided for "id", a new value will be generated
-            and assigned to.
+    def __init__(self, id=None):
         """
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+            This is a constructor function that assigns an ID to an object either
+            based on a given ID or by incrementing a counter.
+
+            Args:
+                id (int): The "id" parameter is an optional argument that can
+                be passed to the constructor of an object. If a value is provided
+                for "id", it will be assigned to the object's "id" attribute.
+                If no value is provided for "id", a new value will be generated
+                and assigned to.
+            """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
