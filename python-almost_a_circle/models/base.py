608aea7fdbf8dc1b801bda0f8aec63dfb10ecc01
ruthfonsecass <7502@holbertonstudents.com>
2024-02-21 16:21:26 +0100

    update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -7,17 +7,19 @@ class Base:
     The goal of it is to manage id attribute in all your future classes and
     to avoid duplicating the same code (by extension, same bugs)
     """
-    __nb_objects = 0
 
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """
-            This is a constructor function that assigns an ID to an object either
+            This is a constructor function that assigns an ID to
+            an object either
             based on a given ID or by incrementing a counter.
 
             Args:
                 id (int): The "id" parameter is an optional argument that can
-                be passed to the constructor of an object. If a value is provided
+                be passed to the constructor of an object. If a value is
+                provided
                 for "id", it will be assigned to the object's "id" attribute.
                 If no value is provided for "id", a new value will be generated
                 and assigned to.
