157114532543331586c5f8c9984cbb6ac26d9497
ruthfonsecass <7502@holbertonstudents.com>
2024-03-21 21:53:05 +0100

    update

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,25 +2,12 @@
 """Define Python interpreter path."""
 
 def add_integer(a, b=98):
-    """Add two integers or floats, converting floats to integers before addition.
+    """Add two integers or floats, converting floats to integers before addition."""
     
-    Args:
-        a: The first number, must be an integer or float.
-        b: The second number, must be an integer or float. Defaults to 98.
-
-    Returns:
-        The sum of a and b, with both treated as integers.
-
-    Raises:
-        TypeError: If either a or b is not an integer or float.
-    """
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
-    elif type(a) is float:
-        a = int(a)
+    if (type(a) is int or type(a) is float):
+        if (type(b) is int or type(b) is float):
+            return int(a) + int(b)
+        else:
+            raise TypeError("b must be an integer")
     else:
-        type(b) is float
-        b = int(b)
-    return a + b
+        raise TypeError("a must be an integer")
