f414cfeea0eaa3841a3af1bb2de8c8b76c0873cb
ruthfonsecass <7502@holbertonstudents.com>
2024-03-21 22:36:05 +0100

    update

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
 -- Write a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-SELECT tv_genres.name AS genres
+SELECT tv_genres.name AS name
+FROM tv_genres
 JOIN tv_show_genres 
         ON genres.id = tv_show_genres.genre_id
 JOIN tv_shows 
--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT tv_shows.title 
+SELECT tv_shows.title  AS title
 FROM tv_shows
     JOIN tv_show_genres 
         ON tv_shows.id = tv_show_genres.show_id
