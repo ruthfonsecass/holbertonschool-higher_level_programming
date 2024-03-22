7b7ff9a142c2102cba78017627bf9c2a34e9c5c6
ruthfonsecass <7502@holbertonstudents.com>
2024-03-22 22:02:51 +0100

    update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -99,3 +99,16 @@ class Rectangle(Base):
                                                        self.__x, self.__y,
                                                        self.__width,
                                                        self.__height)
+
+
+    def to_dictionary(self):
+            """Public method that returns the dictionary representation
+            of a Rectangle
+            """
+            return {
+                "id": self.id,
+                "width": self.width,
+                "height": self.height,
+                "x": self.x,
+                "y": self.y
+            }
