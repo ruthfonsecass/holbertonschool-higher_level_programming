b18a9a18ebb19931c65c6b52d7cf7c54f239b035
ruthfonsecass <7502@holbertonstudents.com>
2024-03-22 22:06:06 +0100

    update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """A module with a simple class"""
+import json
 
 
 class Base:
@@ -30,3 +31,19 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Class method that writes the JSON string representation of
+        list_objs to a file
+        """
+        if list_objs is None:
+            list_objs = []
+
+        my_dict = []
+        for obj in list_objs:
+            my_dict.append(obj.to_dictionary())
+        obj = cls.to_json_string(my_dict)
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            jsonfile.write(obj)
