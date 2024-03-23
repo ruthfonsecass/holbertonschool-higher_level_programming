de8a519514581baf5ad41567ba381e66cffda2b4
ruthfonsecass <7502@holbertonstudents.com>
2024-03-23 11:13:46 +0100

    update

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 letters_delete = [101, 113]
 for i in range(97, 123):
-    letter = chr(i)
-    if letter not in letters_delete:
-        print(letter, end='')
+    if i != 101 and i != 113:
+        print(i, end='')
