8dff3bcac6622ad0e19b87e98d5f148c6e79254b
ruthfonsecass <7502@holbertonstudents.com>
2024-03-23 11:31:38 +0100

    update

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,7 +4,6 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
-            new_list.append(result)
         except TypeError:
             print("wrong type")
             result(0)
