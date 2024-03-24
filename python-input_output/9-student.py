3855ee9c228037dffb55577781d0dfb368ea0301
ruthfonsecass <7502@holbertonstudents.com>
2024-03-24 22:39:33 +0100

    update

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,6 +7,7 @@ and printing the contents of a file.
 
 class Student:
 
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
