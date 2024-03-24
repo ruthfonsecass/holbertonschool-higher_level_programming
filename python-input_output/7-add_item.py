2ec5e6e0e9d8161d51435d9ed119924edd0ada42
ruthfonsecass <7502@holbertonstudents.com>
2024-03-24 22:37:03 +0100

    update

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,14 +6,15 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+
+args = sys.argv[1:]
 filename = 'add_item.json'
+with open(filename, "a+") as f:
+    try:
+        items = load_from_json_file(filename)
+    except Exception:
+        items = []
 
-try:
-    items = load_from_json_file(filename)
-except Exception:
-    items = []
 
-
-items.extend(sys.argv[1:])
-
-save_to_json_file(items, filename)
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
