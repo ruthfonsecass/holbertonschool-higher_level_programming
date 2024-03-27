a3ef2b0174474ad7140efedaec7d118ea71a4a71
ruthfonsecass <7502@holbertonstudents.com>
2024-03-27 16:27:45 +0100

    update

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,6 +7,7 @@ Module composed by a function that prints 2 new lines after ".?:" characters
 def text_indentation(text):
     """ Function that prints 2 new lines after ".?:" characters
     """
+
     if type(text) is not str:
         raise TypeError("text must be a string")
     for i in range(len(text)):
